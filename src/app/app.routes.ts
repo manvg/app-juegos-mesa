9ba50b0ba1b32c0f97c3985179b2a3e68c0f0bc3
Manuel Valdés <manuelvaldesg97@gmail.com>
2024-06-09 18:15:13 -0400

    Nueva página para la categoría "Familiar" de juegos de mesa

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,9 @@
 import { Routes } from '@angular/router';
 import { IndexComponent } from './components/index/index.component';
+import { FamiliarComponent } from './components/familiar/familiar.component';
 
 export const routes: Routes = [
     { path: '', redirectTo: '/index', pathMatch: 'full' },
-    {path: 'index', component: IndexComponent}
+    {path: 'index', component: IndexComponent},
+    {path: 'familiar', component: FamiliarComponent}
 ];
---
+++ src/app/components/familiar/familiar.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-familiar',
+  standalone: true,
+  imports: [CommonModule, RouterModule],
+  templateUrl: './familiar.component.html',
+  styleUrl: './familiar.component.scss'
+})
+export class FamiliarComponent {
+
+}
