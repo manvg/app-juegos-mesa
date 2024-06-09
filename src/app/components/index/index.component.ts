d494338b1b010365efc7093c9c449b4386e1073b
Manuel Valdés <manuelvaldesg97@gmail.com>
2024-06-09 17:49:03 -0400

    1)Configuraciones iniciales bootstrap y assets. 2)Se agregan imagenes para todo el proyecto. 3)Nueva página Index

---
+++ src/app/components/index/index.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-index',
+  standalone: true,
+  imports: [CommonModule, RouterModule],
+  templateUrl: './index.component.html',
+  styleUrl: './index.component.scss'
+})
+export class IndexComponent {
+
+}
