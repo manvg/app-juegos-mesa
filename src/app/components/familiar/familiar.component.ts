d167597c2e02eada6ffdedb5bad35853f59e7bba
Manuel Valdés <manuelvaldesg97@gmail.com>
2024-06-09 23:39:34 -0400

    Se modifica pagina de la categoría "Familia" para cargar de forma dinámica los productos a partir de un array. Se hace uso de ngFor y ngIf.

--- src/app/components/familiar/familiar.component.ts
+++ src/app/components/familiar/familiar.component.ts
@@ -10,5 +10,27 @@ import { RouterModule } from '@angular/router';
   styleUrl: './familiar.component.scss'
 })
 export class FamiliarComponent {
-  
+  productos = [
+    {
+      imagen: 'assets/images/familiar_1.png',
+      titulo: 'Exit Kids: Acertijos en la Jungla',
+      descripcion: 'Juego de escape cooperativo',
+      precio: 15000,
+      precioOferta: null
+    },
+    {
+      imagen: 'assets/images/familiar_2.png',
+      titulo: 'Fantasma Blitz',
+      descripcion: '5 piezas, 60 cartas con ilustraciones.',
+      precio: 12990,
+      precioOferta: 24990
+    },
+    {
+      imagen: 'assets/images/familiar_3.png',
+      titulo: '¡Rescate!',
+      descripcion: 'Es un juego cooperativo de lucha contra el fuego.',
+      precio: 22990,
+      precioOferta: 32000
+    }
+  ];
 }
