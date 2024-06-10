4b367397b97d4c21ea80dc8acf6f016489149413
Manuel Valdés <manuelvaldesg97@gmail.com>
2024-06-10 00:54:09 -0400

    Se aplica carga dinámica de productos a partir de array para las páginas con categoria de "Aventura" e "Infantil"

--- src/app/components/aventura/aventura.component.ts
+++ src/app/components/aventura/aventura.component.ts
@@ -10,5 +10,27 @@ import { RouterModule } from '@angular/router';
   styleUrl: './aventura.component.scss'
 })
 export class AventuraComponent {
-
+  productos = [
+    {
+      imagen: 'assets/images/aventura_1.png',
+      titulo: 'Almanac: El Camino del Dragón',
+      descripcion: 'Tu caravana está lista y los vagones ordenados. El camino que tienes por delante está plagado de peligros, pero ése es el precio de una fortuna inimaginable.',
+      precio: '34.990',
+      precioOferta: '44.000'
+    },
+    {
+      imagen: 'assets/images/aventura_2.png',
+      titulo: 'Wonder Book',
+      descripcion: 'Wonder Book es un juego cooperativo y narrativo. Sumérgete en la aventura y descubre los diferentes rumbos que puede tomar la historia. ¡Cada decisión tendrá su consecuencia!',
+      precio: '44.990',
+      precioOferta: '61.990'
+    },
+    {
+      imagen: 'assets/images/aventura_3.png',
+      titulo: 'Tough Calls: Dystopia',
+      descripcion: 'Tough Calls: Dystopia es un juego narrativo y argumentativo ambientado en una variedad de escenarios construidos desde la Ciencia Ficción, como una invasión zombie...',
+      precio: '16.990',
+      precioOferta: null
+    }
+  ];
 }
--- src/app/components/infantil/infantil.component.ts
+++ src/app/components/infantil/infantil.component.ts
@@ -10,5 +10,27 @@ import { RouterModule } from '@angular/router';
   styleUrl: './infantil.component.scss'
 })
 export class InfantilComponent {
-
+  productos = [
+    {
+      imagen: 'assets/images/infantil_1.png',
+      titulo: 'Taco Gatito Pizza',
+      descripcion: 'Este juego de cartas requiere de toda tu atención y rapidez. ¡Sobre todo rapidez!',
+      precio: '11.990',
+      precioOferta: null
+    },
+    {
+      imagen: 'assets/images/infantil_2.png',
+      titulo: '¿Quién Fue?',
+      descripcion: 'Compiten para deshacerse de sus cartas y así evitar la culpa de ser el dueño del animal que se hizo caca.',
+      precio: '12.990',
+      precioOferta: null
+    },
+    {
+      imagen: 'assets/images/infantil_3.png',
+      titulo: 'Mindo: Perros',
+      descripcion: 'Juego de lógica para niños. Puzzles multinivel, completar el cuadrado con las piezas dadas.',
+      precio: '10.000',
+      precioOferta: null
+    }
+  ];
 }
